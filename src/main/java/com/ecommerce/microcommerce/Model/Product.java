fa53d201078ea3f80cbf833c4227677e4a2ed085
benzarty <m.benzarti.1996@gmail.com>
2020-01-06 14:44:44 +0100

    alou

--- src/main/java/com/ecommerce/microcommerce/Model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/Model/Product.java
@@ -1,6 +1,82 @@
 package com.ecommerce.microcommerce.Model;
 
 public class Product {
+    private int id;
+    private boolean transaction;
+    private  float montant;
+    private String productname;
+    private int quantite;
+    private int clientid;
+    private float productpriceunity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(boolean transaction) {
+        this.transaction = transaction;
+    }
+
+    public float getMontant() {
+        return montant;
+    }
+
+    public void setMontant(float montant) {
+        this.montant = montant;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public int getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(int clientid) {
+        this.clientid = clientid;
+    }
+
+    public float getProductpriceunity() {
+        return productpriceunity;
+    }
+
+    public void setProductpriceunity(float productpriceunity) {
+        this.productpriceunity = productpriceunity;
+    }
+
+    public Product(int id, boolean transaction, float montant, String productname, int quantite, int clientid, float productpriceunity) {
+        this.id = id;
+        this.transaction = transaction;
+        this.montant = montant;
+        this.productname = productname;
+        this.quantite = quantite;
+        this.clientid = clientid;
+        this.productpriceunity = productpriceunity;
+    }
+
+    public Product() {
+    }
 
 }
 
--- src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
@@ -8,6 +8,39 @@ import com.ecommerce.microcommerce.Model.Product;
 import java.util.List;
 
 
+@RestController
 public class ProductController {
+    //@Autowired afin que Spring se charge d'en fabriquer une instance. ProductDao a désormais accès à toutes
+    // les méthodes que nous avons définies
+    @Autowired
+    private ProductDao productDao;
 
+    //@RequestMapping(value="/Produitss", method= RequestMethod.GET)
+    @GetMapping("/Produitss")
+    public String listeProduitss() {
+        return "Un exemple de produit";
+    }
+
+
+    @GetMapping(value = "/Produitss/{id}")
+    public String afficherUnProdui(@PathVariable int id) {
+        return "Vous avez demandé un produit avec l'id  " + id;
+    }
+
+
+    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
+    public List<Product> listeProduits() {
+        return productDao.findAll();
+    }
+
+    //Récupérer un produit par son Id
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        return productDao.findById(id);
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody Product a) {
+        productDao.save(a);
+    }
 }
